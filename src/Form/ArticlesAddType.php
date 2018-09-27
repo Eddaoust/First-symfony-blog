a59d31e2def8badc4498cdb0e7da3750bb6804a8
Eddaoust <edmond.daoust@gmail.com>
2018-09-27 11:41:45 +0200

    recherche pour récupérer l'id utilisateur dans un champ caché via le form entity Article

--- src/Form/ArticlesAddType.php
+++ src/Form/ArticlesAddType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\Articles;
 use App\Entity\Category;
 use App\Entity\User;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,18 +16,19 @@ class ArticlesAddType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = $options['user_id'];
+
         $builder
             ->add('title')
             ->add('content')
             ->add('img')
-            ->add('category', EntityType::class, [
-                'class' => Category::class,
-                'choice_label' => 'title'
+            ->add('category', EntityType::class, [ // Champ du formulaire issu d'une entité
+                'class' => Category::class, // Sélection de la classe
+                'choice_label' => 'title' // Sélection du champ
             ])
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'username',
-                'disabled' => true
+            ->add('user', HiddenType::class, [ // Recherche d'une solution afin de passer l'id utilisateur en hidden
+                'data' => $user,
+                'data_class' => null
             ])
         ;
     }
